f35d201362e2edb4301b26be1404e0e6b19052af
TomTheTornado <tksp98@gmail.com>
2022-08-15 23:12:55 -0500

    Added batman terms

--- ipsum.js
+++ ipsum.js
@@ -56,7 +56,7 @@ app.post('/', function (request, response) {
 	    sentenceLength,
 	    term;
 	for (i = 0; i < paragraphCount; i++) {
-		paragraph = startWith ? 'Live ipsum long and prosper dolor where no one sit amet has gone before ' : '';
+		paragraph = startWith ? 'Holy ipsum batman ' : '';
 		while (paragraph.length < paragraphLength) {
 			sentenceLength = startWith ? 10 : random.getRandomInteger(4, 10);
 			for (j = 0; j < sentenceLength; j++) { 
